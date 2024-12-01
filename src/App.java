e29be11e599663570b2a04d52d6b9882eee9a522
aLieexe <rizky.alay@gmail.com>
2024-12-01 17:09:56 +0700

    test

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args){
         Game game = new Game();
         game.start();
-        System.out.println("===== code by: Galang Krisna Pramudya =====");
+        System.out.println("===== code by: Galang Krisna Pramudya hitam =====");
     }
 }
