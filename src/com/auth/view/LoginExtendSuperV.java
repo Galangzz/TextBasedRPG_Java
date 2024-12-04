0e186a2c786ec5dd060efffbcab978ecedf968ed
Galangzz <galangpro280605@gmail.com>
2024-12-05 06:17:04 +0700

    update kondisi LoginExtendSuperV

--- src/com/auth/view/LoginExtendSuperV.java
+++ src/com/auth/view/LoginExtendSuperV.java
@@ -9,22 +9,23 @@ public class LoginExtendSuperV extends SuperV {
     
     public void viewLogin(){
         String msg = "";
-        do { 
             
-            view();
-            String email = getEmail();
-            String password = getPassword();
-            int a = DB.cariData(email, password);
-            if (a > 0){
-                msg = DB.authAkun(a, email, password);
-                System.out.println(msg);
-            }else{
-                System.out.println("Account is not registered yet");
-            }
-        } while (!msg.equals("Login Success"));
-        //langsung keprogram
-        Game game = new Game();
+        view();
+        String email = getEmail();
+        String password = getPassword();
+        int a = DB.cariData(email, password);
+        if (a > 0){
+            msg = DB.authAkun(a, email, password);
+            System.out.println(msg);
+        }else{
+            System.out.println("Account is not registered yet");
+        }
         
-        game.start();
+        if (msg.equals("Login Success")){
+            //langsung keprogram
+            Game game = new Game();
+            
+            game.start();
+        }
     }
 }
