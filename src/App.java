90457d3332c0b5df6d915fff5ade0b957f452403
Galangzz <galangpro280605@gmail.com>
2025-01-03 12:44:36 +0700

    update nama kelompok

--- src/App.java
+++ src/App.java
@@ -7,6 +7,19 @@ public class App {
         
         AuthView initialDisplay = new AuthView();
         initialDisplay.displayMenu(); 
-        PrintDelay.print("===== code by: Kelompok 1 TID 23=====");
+        PrintDelay.print("\n\n===== code by: Kelompok 1 TID 23 =====\n");
+        PrintDelay.print(
+            """
+
+            Anggota :\n
+            | Nama                     | NIM         | 
+            |--------------------------|-------------| 
+            | Galang Krisna Pramudya   | 23051204134 | 
+            | Alif Rasyid Febriansyah  | 23051204131 | 
+            | Mirza Faris Al Arifin    | 23051204142 | 
+            | Alvin Aulia Rahman Sh    | 23051204117 | 
+            | Mohammad Eksa Hardian    | 23051204115 | 
+            | Neng Jihan               | 23051204114 | 
+            """);
     }
 }
