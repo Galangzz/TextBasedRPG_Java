a5587eff1eb346bee539486d343f2ca2e5dbf9c9
Galangzz <galangpro280605@gmail.com>
2025-01-04 18:48:22 +0700

    update public

--- src/com/model/Equipment.java
+++ src/com/model/Equipment.java
@@ -3,12 +3,12 @@ package com.model;
 interface Equipment {
 
     // mengambil dan set name pada weapon atau armor
-    String getName();
-    void setName(String name);
+    public String getName();
+    public void setName(String name);
 
     //mengambil dan set damage pada weapon atau defPower pada armor
-    int getValue();
-    void setValue(int value);
+    public int getValue();
+    public void setValue(int value);
 
     //menampilkan statistik dari armor atau weapon(nama, damage atau def)
     public void showStatistik();
